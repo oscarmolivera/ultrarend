cf99d84956b72e5b55b49d86fa67087c2684702f
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-09-03 11:39:29 -0400

    Usuario Tipo ADMIN +Seed, +Spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(email: "test@test.com", 
+                        first_name: "Jon", 
+                        last_name: "Snow",                    
+                        password: "asdfasdf", 
+                        password_confirmation: "asdfasdf",  
+                        )
+  end
+  describe "creation" do
+
+    it "can be created" do
+      expect(@user).to be_valid
+    end
+
+  end
+
+  describe 'validations' do
+    
+    it "cannot be created without first_name, last_name" do
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+    
+  end
 end
