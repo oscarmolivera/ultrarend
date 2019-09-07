c2ba2f2e94c65edc7d4d6c4a1e894584f1dd21e7
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-09-07 14:04:16 -0400

    Remocion de HOME Controller - Devise Registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'dashboards/index'
-  devise_for :users
+  devise_for :users, skip: %i[registration]
   #root 'home#index'
   devise_scope :user do
     root :to => 'devise/sessions#new'
